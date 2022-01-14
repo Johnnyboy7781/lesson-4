d06b227d49759bb390e002e876788f7a3cffe02f
Jonathan McDonnell <mcdonnell.jonathan@yahoo.com>
2022-01-13 20:56:05 -0500

    init

--- 04-Web-APIs/01-Activities/04-Data-Attributes/Unsolved/assets/js/script.js
+++ 04-Web-APIs/01-Activities/04-Data-Attributes/Unsolved/assets/js/script.js
@@ -4,4 +4,11 @@ container.addEventListener("click", function(event) {
   var element = event.target;
 
   // TODO: Complete function
+  if (element.getAttribute("data-state") === "hidden") {
+    element.textContent = element.getAttribute("data-number");
+    element.setAttribute("data-state", "visible");
+  } else if (element.getAttribute("data-state") === "visible") {
+    element.textContent = "";
+    element.setAttribute("data-state", "hidden");
+  }
 });
